8ffe95de98c9be119fa84b5b884b1b7423996098
Guilherme Menezes Silva <profissional.guilherme.menezes@gmail.com>
2025-10-16 18:15:57 -0300

    Banco-Dados-Biblioteca #1.4
    Alteração do executável de python para criação das funções (~/data/app.py).

--- data/app.py
+++ data/app.py
@@ -1,16 +1,34 @@
 import mysql.connector
 
 class SQL:
-    sql = mysql.connector.connect(
+    _sql = mysql.connector.connect(
             user="root",
             password="552299",
             host="localhost",
             database="biblioteca" 
     )
+
+class Database:
     def select_view(table):
-        cursor = SQL.sql.cursor()
+        cursor = SQL._sql.cursor()
         cursor.execute(f"SELECT * FROM {table}")
         myresult = cursor.fetchall()
         for data in myresult:
             print(data)
+    def view_tables():
+        cursor = SQL._sql.cursor()
+        cursor.execute("""
+            SELECT TABLE_NAME, TABLE_TYPE
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = DATABASE();
+        """)
+        myresult = cursor.fetchall()
+        for name, type_ in myresult:
+            if type_ == "VIEW":
+                print(f"{name} - View")
+            elif type_ == "BASE TABLE":
+                print(f"{name} - Tabela")
 
+class Login:
+
+class Users:
